7b5239c952e44f2e52b498d4ad3148c15734807f
Sam Chelgut <samchelgut@gmail.com>
2024-04-04 21:49:13 +0300

    assignment done, tests passing

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//multiply to 62
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+// Random number greater than 0
+const randomDecimal = Math.random()
+const randomWithinRange = randomDecimal * 100;
+const random = Math.ceil(randomWithinRange);
+// console.log(randomDecimal)
+
+// Modulus
+let num3 = 9
+let num4 = 5
+
+const mod = num3%num4
+
+// find max in a given set
+let set = (1,4,6,19,5,10,20)
+const max = Math.max(set)
+
+// console.log(max)
